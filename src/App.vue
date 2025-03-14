d86ab582cb10f8b8b5c95429bb08fc4371240a04
gtopsis <csd3004@csd.uoc.gr>
2025-03-14 22:03:25 +0200

    feat(defaultLayout): create default page layout of app
    contains: topbar, routes' pages

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
+import DefaultLayout from './layouts/DefaultLayout.vue'
 </script>
 
 <template>
-  <RouterView />
+  <DefaultLayout>
+    <RouterView />
+  </DefaultLayout>
 </template>
 
 <style scoped></style>
---
+++ src/layouts/DefaultLayout.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import TheTopBar from '../components/TheTopBar.vue'
+</script>
+
+<template>
+  <div class="flex h-screen w-full flex-col">
+    <header class="sticky w-full">
+      <slot name="header">
+        <TheTopBar />
+      </slot>
+    </header>
+
+    <main class="w-full flex-1 overflow-y-auto">
+      <slot></slot>
+    </main>
+  </div>
+</template>
+
+<style lang="css" scoped></style>
