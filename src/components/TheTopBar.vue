d397c7bfa15f40ad2781c93ee96db9e38024fedc
gtopsis <csd3004@csd.uoc.gr>
2025-03-15 18:08:58 +0200

    fix(TheTopBar): fix typo when using component RouterLink

--- src/components/TheTopBar.vue
+++ src/components/TheTopBar.vue
@@ -8,38 +8,37 @@ interface NavItem {
 
 const navItems = ref<NavItem[]>([
   { title: 'New', routeName: 'home' },
-  { title: 'Favorites', routeName: 'home' },
+  { title: 'Favorites', routeName: 'favorites' },
 ])
 </script>
 
 <template>
   <nav class="container mx-auto flex items-center justify-center">
-    <routerLink
+    <RouterLink
       :to="{ name: navItems[0].routeName }"
       class="text-accent hover:border-b-accent border-b-2 border-transparent px-2 py-2 font-bold no-underline transition-[border]"
     >
       New Jokes
-    </routerLink>
+    </RouterLink>
 
-    <routerLink
+    <RouterLink
       :to="{ name: navItems[0].routeName }"
       class="title mx-4 border-b-2 border-transparent px-4 py-2 font-bold no-underline hover:border-b-2"
     >
       <strong class="font-bellota text-secondary text-3xl">JokeBox</strong>
-    </routerLink>
+    </RouterLink>
 
-    <routerLink
+    <RouterLink
       :to="{ name: navItems[1].routeName }"
       class="text-accent hover:border-b-accent border-b-2 border-transparent px-2 py-2 font-bold no-underline"
-      >Favorites</routerLink
+      >Favorites</RouterLink
     >
   </nav>
 </template>
 
 <style lang="css" scoped>
 nav a:not(.title).router-link-active {
-  width: auto;
-  border-bottom: 2px solid var(--color-accent);
+  border-bottom-color: var(--color-accent);
 }
 
 nav a:hover {
