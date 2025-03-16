59c9846bdb9d124c1aef4ea78071bc2655d31b3f
gtopsis <csd3004@csd.uoc.gr>
2025-03-16 18:54:22 +0200

    feat(home): add list of jokes, with  placeholder on pending state and alert when error occurred

--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -10,7 +10,7 @@ import TheTopBar from '../components/TheTopBar.vue'
       </slot>
     </header>
 
-    <main class="min-h-[80vh] w-full flex-1 grow">
+    <main class="min-h-[85vh] w-full flex-1 grow">
       <slot></slot>
     </main>
   </div>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,40 @@
 <script setup lang="ts">
-import HomeToolBar from '../components/GetJokesToolbar.vue'
+import TheCardSkeleton from '@/components/TheCardSkeleton.vue'
+import { useFetch } from '@/composables/useFetch'
+import type { Joke } from '@/types/joke'
+import { computed, onMounted } from 'vue'
+import ErrorAlert from '../components/ErrorAlert.vue'
+import GetJokesToolbar from '../components/GetJokesToolbar.vue'
+import JokeCollection from '../components/JokeCollection.vue'
+
+const jokeType = 'random'
+const numberOfJokes = 25
+const apiUrl = `${import.meta.env.VITE_API_BASE_URL}${jokeType}/${numberOfJokes}`
+const { fetchData, isFetching, error, data } = useFetch<Joke[]>(apiUrl)
+
+const isLoading = computed(() => isFetching.value)
+const fetchError = computed<Error | undefined>(() =>
+  error.value
+    ? new Error('Failed to fetch new jokes. Please try again!')
+    : undefined
+)
+const jokes = computed<Joke[] | null>(() => data.value)
+
+const fetchJokes = async () => {
+  await fetchData()
+}
+
+onMounted(async () => {
+  await fetchJokes()
+})
 </script>
 
 <template>
-  <HomeToolBar @onJobsFetchRequested="($event) => console.log($event)" />
+  <GetJokesToolbar @onJobsFetchRequested="fetchJokes" />
+
+  <div class="mt-6 w-full">
+    <TheCardSkeleton v-if="isLoading" />
+    <ErrorAlert v-else-if="fetchError" :error="fetchError" />
+    <JokeCollection v-else-if="jokes !== null" :jokes="jokes" />
+  </div>
 </template>
