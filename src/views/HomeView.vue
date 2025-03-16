45a155bf2c7475d9284dc681420910eecebd9aa1
gtopsis <csd3004@csd.uoc.gr>
2025-03-16 02:15:59 +0200

    feat(Home): add getjokes toolbar in Home page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,7 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import HomeToolBar from '../components/GetJokesToolbar.vue'
+</script>
 
 <template>
-  <main></main>
+  <HomeToolBar @onJobsFetchRequested="($event) => console.log($event)" />
 </template>
