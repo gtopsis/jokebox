071ec1dca0ae1f2b50a68fd74f2ea738658f2f30
gtopsis <csd3004@csd.uoc.gr>
2025-03-16 18:49:35 +0200

    feat(loading components): add circular spinner and card skeleton components

---
+++ src/components/TheCardSkeleton.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="mx-auto flex w-full justify-center">
+    <div role="status" class="flex min-w-md animate-pulse">
+      <div class="mt-2 ml-4 w-full">
+        <strong
+          class="mb-4 h-3 w-48 rounded-full bg-gray-300 text-3xl"
+        ></strong>
+        <p class="mb-2.5 h-2 w-[320px] rounded-full bg-gray-300"></p>
+        <p class="mb-2.5 h-2 w-[320px] rounded-full bg-gray-300"></p>
+      </div>
+      <div class="flex-shrink-0">
+        <span
+          class="flex h-12 w-12 items-center justify-center rounded-full bg-gray-300"
+        ></span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="css" scoped></style>
