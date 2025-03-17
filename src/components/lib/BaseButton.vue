2bae52341af0180e291c6599d243fa1ab90b23de
gtopsis <csd3004@csd.uoc.gr>
2025-03-17 20:47:08 +0200

    feat(BaseButton): add new type of text button

--- src/components/lib/BaseButton.vue
+++ src/components/lib/BaseButton.vue
@@ -1,11 +1,10 @@
 <script setup lang="ts">
+type ButtonType = 'regular' | 'icon' | 'text'
 interface Colors {
   background?: string
   border?: string
   text?: string
 }
-
-type ButtonType = 'regular' | 'icon'
 
 interface Props {
   id?: string
@@ -27,15 +26,18 @@ const emit = defineEmits<{ (e: 'click', event: Event): void }>()
 <template>
   <button
     :id="id"
-    class="inline-block cursor-pointer text-center text-sm font-semibold shadow-[0_.2rem_0.3rem_-.25rem_black] transition-transform duration-250 hover:scale-105 focus-visible:ring-2 focus-visible:ring-zinc-700 focus-visible:ring-offset-2 focus-visible:outline-none active:translate-y-[1px] active:shadow-none"
+    class="inline-block cursor-pointer text-center text-sm font-semibold transition-transform duration-250 hover:scale-105 focus-visible:ring-2 focus-visible:ring-zinc-700 focus-visible:ring-offset-2 focus-visible:outline-none active:translate-y-[1px] active:shadow-none"
     :title="type === 'icon' ? title : undefined"
     :aria-label="title"
+    :class="{
+      'shadow-[0_.2rem_0.3rem_-.25rem_black]': type !== 'text',
+      'rounded-full p-2': type === 'icon',
+      'rounded-md px-4 py-2': type !== 'icon',
+    }"
     :style="{
       backgroundColor: colors?.background ?? 'var(--color-zinc-900)',
       color: colors?.text ?? 'var(--color-white)',
       borderColor: colors?.border ?? 'var(--color-zinc-900)',
-      borderRadius: type === 'icon' ? '100%' : 'var(--radius-sm)',
-      padding: type === 'icon' ? '0.5rem' : '0.5rem 1rem',
     }"
     @click="emit('click', $event)"
   >
