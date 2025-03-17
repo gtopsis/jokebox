fae26a389c9161d1cb599d59c8bd1e00191ac929
gtopsis <csd3004@csd.uoc.gr>
2025-03-17 07:20:18 +0200

    perf(router): lazy-load notFountError component for relative route

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,3 @@
-import NotFoundErrorView from '@/views/NotFoundErrorView.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
@@ -13,15 +12,12 @@ const router = createRouter({
     {
       path: '/favorites',
       name: 'favorites',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () => import('../views/FavoritesView.vue'),
     },
     {
       path: '/:pathMatch(.*)*',
       name: 'NotFound',
-      component: NotFoundErrorView,
+      component: () => import('../views/NotFoundErrorView.vue'),
     },
   ],
 })
