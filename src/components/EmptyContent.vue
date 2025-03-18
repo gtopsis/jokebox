a7d2c3b0a14be4a9a3f5b288eb62368727322431
gtopsis <csd3004@csd.uoc.gr>
2025-03-18 09:10:12 +0200

    feat(EmptyContent): add component to show proper message when there is no content

---
+++ src/components/EmptyContent.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="w-full text-center">
+    <span class="text-text-secondary text-md mx-2 mt-4 mb-0 block">
+      <slot>No content</slot>
+    </span>
+  </div>
+</template>
+
+<style lang="css" scoped></style>
