ca5e81b2cb3f30b7b39b4f30ad1e1e15a668023b
gtopsis <csd3004@csd.uoc.gr>
2025-03-22 22:01:21 +0200

    chore(deps): add Pinia pkg for state management

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import '@/assets/css/main.css'
 
 import App from '@/App.vue'
+import { createPinia } from 'pinia'
 import { createApp } from 'vue'
 import router from './router'
 
 const app = createApp(App)
 
 app.use(router)
+app.use(createPinia())
 
 app.mount('#app')
